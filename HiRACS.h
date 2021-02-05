287dd2e4bd4bdb394694135f93e3bebe9ceec7fc
Ken Ramey <ken@freedomcp.com>
2021-02-05 15:38:27 -0700

    Update HiRACS.h

--- HiRACS.h
+++ HiRACS.h
@@ -14,12 +14,14 @@
 using namespace std;
 
 class HiRACS {
+    // Wrapper for the entire camera system. As new components are added, they will be
+    // instantiated here.
     private:
         int _masterSocket;
         fd_set _readfds;
         std::array<int, 5> _client_socket = {0};
         const int _max_clients = 5;
-        Focuser _wfovFocuser;
+        Focuser _wfovFocuser;                       // Wide field-of-view focuser. A separate controller will be added for narrow field
         SignalHandler _signalHandler;
         char * prepareResponse(string);
 
